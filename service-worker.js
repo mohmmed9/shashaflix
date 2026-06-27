4e0f1da9d7bdf26c6f0f32e99eefd88df21b68df
mohmmed9 <63237432+mohmmed9@users.noreply.github.com>
2026-06-27 17:59:54 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-});
-
-self.addEventListener('fetch', (event) => {
-  // هذا الكود ضروري ليعتبره المتصفح تطبيقاً حقيقياً
-});
